3abe6d18837a45eee1b7ff056e97423eb6c4f434
Erick Rodriguez Rodriguez <42397195+ErickRodz@users.noreply.github.com>
2021-03-13 23:34:06 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from handler.PatientHandler import PatientHandler
+from flask_cors import CORS
+
+# Activate
+app = Flask(__name__)
+# Apply CORS to this app
+CORS(app)
+
+
+@app.route('/')
+def greeting():
+    return 'Hello, this is the DevOps App!'
+
+@app.route('/patients', methods=['GET', 'POST'])
+def patients():
+    if request.method == 'POST':
+        print("REQUEST: ", request.json)
+        return PatientHandler().insertPatientJson(request.json)
+    else:
+        if not request.args:
+            return PatientHandler().getAllPatients()
+
+@app.route('/doctors', methods=['GET', 'POST'])
+def doctors():
+    if request.method == 'POST':
+        print("REQUEST: ", request.json)
+        return DoctorHandler().insertDoctorJson(request.json)
+    else:
+        if not request.args:
+            return DoctorHandler().getAllDoctor()
+
+@app.route('/appointment', methods=['GET', 'POST'])
+def appointment():
+    if request.method == 'POST':
+        print("REQUEST: ", request.json)
+        return AppointmentHandler().insertAppointmentJson(request.json)
+    else:
+        if not request.args:
+            return AppointmentHandler().getAllAppointments()
